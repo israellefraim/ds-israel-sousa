814b65216a679570c8ac40ca8a577801ace7d258
israellefraim <israell.efraim@gmail.com>
2025-03-24 09:18:58 -0300

    .

---
+++ variaveis-estruturas-condicionais/Ex07.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex07 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("x = ");
+        float primeiroNum = input.nextFloat();
+
+        System.out.print("x - 1 = " + (primeiroNum - 1));
+
+    }
+}
