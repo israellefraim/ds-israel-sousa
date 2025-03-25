e1368b4f1bf02a3b378fababd39d4d7796759013
israellefraim <israell.efraim@gmail.com>
2025-03-25 10:58:31 -0300

    Upload Ex09
    Modifique o exercício anterior para aceitar somente valores maiores que 0 para N. Caso o valor informado (para N) não seja maior que 0, deverá ser lido um novo valor para N.

---
+++ lacos-repeticao/Ex09.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ex09
+{
+	public static void main(String[] args) {
+	    int valorDelimitador;
+	    Scanner input = new Scanner(System.in);
+	    
+	    do {
+	        System.out.print("Insira o valor do delimitador: ");
+	        valorDelimitador = input.nextInt();
+	        if (valorDelimitador <= 0) {
+	            System.out.println("\n-O VALOR INSERIDO DEVE SER MAIOR QUE 0-\n");
+	        }
+	    } while (valorDelimitador <= 0);
+	    
+	    
+	    for (int i = 1; i <= valorDelimitador; i++) {
+	        System.out.println(i);
+	    }
+	}
+}
