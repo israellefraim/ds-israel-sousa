a6f99fe9ec9ef45b170cd88888a769c70754a898
israellefraim <israell.efraim@gmail.com>
2025-03-31 10:56:21 -0300

    Upload Ex11
    Ler um valor inteiro (aceitar somente valores entre 1 e 10) e escrever a tabuada de 1 a 10 do valor lido

---
+++ lacos-repeticao/Ex11.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ex11
+{
+	public static void main(String[] args) {
+	    int valorInteiro, i;
+	    
+	    Scanner input = new Scanner(System.in);
+	    
+	    do {
+	        System.out.print("Insira um valor inteiro: ");
+	        valorInteiro = input.nextInt();
+	        
+	        if (valorInteiro < 1 || valorInteiro > 10) {
+	            System.out.println("\n-INSIRA UM VALOR ENTRE 1 E 10-\n");
+	        }
+	        
+	    } while (valorInteiro < 1 || valorInteiro > 10);
+	    
+	    for (i = 1; i <= 10; i++) {
+	        System.out.println(valorInteiro + " * " + i + " = " + valorInteiro * i);
+	    }
+	}
+}
