386ea30c5d160c755f218e6bbe6e66c50ffeed4f
israellefraim <israell.efraim@gmail.com>
2025-04-04 11:21:22 -0300

    Upload Ex14
    A série de Fibonacci é formada pela sequência 0,1,1,2,3,5,8,13,21,34,55,... Faça um programa que gere a série até que o valor seja maior que 500.

---
+++ lacos-repeticao/Ex14.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Ex14
+{
+	public static void main(String[] args) {
+	    ArrayList<Integer> lista = new ArrayList<>();
+	    
+	    lista.add(0);
+	    lista.add(1);
+	    int i = 1;
+	    
+	    do {
+	        i++;
+	        lista.add(lista.get(i-1) + lista.get(i-2));
+	    } while (lista.get(i) <= 500);
+	    
+	    for (int num : lista) {
+	        System.out.print(num + " ");
+	    }
+	}
+}
