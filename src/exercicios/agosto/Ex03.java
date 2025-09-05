d7d69453e4fdc490a8ef6b9ca6889ff84f68f270
israellefraim <israell.efraim@gmail.com>
2025-09-04 21:39:54 -0300

    Cria Ex03

---
+++ src/exercicios/agosto/Ex03.java
@@ -0,0 +1,16 @@
+package exercicios.agosto;
+
+import java.util.Scanner;
+
+public class Ex03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Insira uma temperatura em ºC: ");
+        int temperaturaCelsius = input.nextInt();
+
+        double temperaturaFahrenheit = (temperaturaCelsius * (9/5) + 32);
+
+        System.out.println(temperaturaCelsius + "ºC = " + temperaturaFahrenheit + "ºF");
+    }
+}
