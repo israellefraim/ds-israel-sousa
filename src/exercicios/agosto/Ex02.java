2d26f2d4f900242b2deed16d8369342ac2511b7a
israellefraim <israell.efraim@gmail.com>
2025-09-04 21:24:59 -0300

    Cria Ex02

---
+++ src/exercicios/agosto/Ex02.java
@@ -0,0 +1,10 @@
+package exercicios.agosto;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        int primeiroNumero = 10;
+        int segundoNumero = 2;
+
+        System.out.println("Soma = ".concat(Integer.toString(primeiroNumero + segundoNumero)));
+    }
+}
