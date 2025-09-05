eed6d6a13cd781788172ebef8917280bb68b9626
israellefraim <israell.efraim@gmail.com>
2025-09-04 21:15:41 -0300

    Cria Ex01

---
+++ src/exercicios/agosto/Ex01.java
@@ -0,0 +1,11 @@
+package exercicios.agosto;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        String nome = "Israel Efraim";
+        String cidade = "São Paulo";
+        int idade = 17;
+
+        System.out.println("O aluno " + nome + " mora em " + cidade + " e tem " + idade + " anos.");
+    }
+}
